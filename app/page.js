a65d213c10647c2f21940649d6ae0256dcf5100a
lellis09 <lishia.ellis09@gmail.com>
2024-03-12 15:26:00 -0400

    made styling changes

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import './globals.css'
 import profilePic from '../public/20210327_150727(1).jpg'
+import Logo from '../public/dsf-logo.svg'
 
 export default function Home() {
   return (
@@ -8,27 +9,31 @@ export default function Home() {
       {/* <div className='bg-lightgreen h-40 w-full'>
       </div> */}
       <section className='flex flex-col  items-center mt-10 lg:mt-16 pt-12 w-full md:w-1/2'>
-        <div className='rounded-full bg-teal-500 w-32 h-32 lg:w-48 lg:h-48 border-2 overflow-hidden relative'>
+        <div className='border-2'>
           <Image 
-            src={profilePic}
-            alt= "picture of the owner of page"
-            className='rounded-full'  
-            layout='fill'
+            src={Logo}
+            alt= "logo of the business"
+            // className='rounded-full'  
+            // layout='fill'
+            width='200rem'
             style={{
-              objectFit:'cover',
+              objectFit:'contain',
             }}    
           />
         </div>
-          <h1 className='text-mywhite mt-4 text-5xl'>Lishia Ellis</h1>
-          <p className='text-mywhite text-lg mt-4 mb-10'>Web Design | Web Development | UX/UI</p>
+         <div className='z-10 bottom-10'>
+         <h1 className='text-lightBlue mt-4 text-5xl'>Lishia Ellis</h1>
+          <p className='text-lightBlue text-sm sm:text-lg mt-4 mb-10'>Web Design | UX/UI | Branding</p>
+         </div>
+          
           <div className='flex flex-col gap-5'>
             <a href='https://twitter.com/TealTernary'target='_blank' rel='noreferrer'>
-            <button className='text-lg uppercase border-2 border-myTeal text-white w-48 h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
+            <button className='text-lg uppercase border-2 border-myTeal shadow-custom-teal shadow-myTeal/30  text-lightBlue w-80 lg:w-[30rem] h-12 flex justify-center items-center hover:bg-myTeal/30 hover:text-white'type='button' >
               Twitter
               </button>
            </a>
            <a href='https://github.com/lellis09'target='_blank' rel='noreferrer'>
-            <button className='text-lg uppercase border-2 border-myTeal text-white w-48 h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
+            <button className='text-lg uppercase border-2 border-myindigo shadow-custom-purple shadow-myTeal text-lightBlue w-80 lg:w-[30rem] h-12 flex justify-center items-center hover:bg-myindigo/30 hover:text-white'type='button' >
               Github
               </button>
            </a>
@@ -38,12 +43,12 @@ export default function Home() {
               </button>
            </a> */}
           <a href='https://www.linkedin.com/in/lishia-ellis/'target='_blank' rel='noreferrer'>
-            <button className='text-lg uppercase border-2 border-myTeal text-white w-48 h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
+            <button className='text-lg uppercase border-2 border-hotpink shadow-custom-pink shadow-myTeal text-lightBlue w-80 lg:w-[30rem] h-12 flex justify-center items-center hover:bg-hotpink/30 hover:text-white'type='button' >
               Linkedin
               </button>
            </a>
            <a href='https://hashnode.com/@tealternary'target='_blank' rel='noreferrer'>
-            <button className='text-lg uppercase border-2 border-myTeal text-white w-48 h-12 flex justify-center items-center hover:bg-pos-100 hover:text-white'type='button' >
+            <button className='text-lg uppercase border-2 border-lightgreen shadow-custom-lime shadow-myTeal text-lightBlue w-80 lg:w-[30rem] h-12 flex justify-center items-center hover:bg-lightgreen/30 hover:text-white'type='button' >
               Hashnode
               </button>
            </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,28 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    backgroundSize: {
-      'size-200': '200% 200%',
-    },
-  backgroundPosition: {
-    'pos-0': '0% 0%',
-    'pos-100': '100% 100%',
+  //   backgroundSize: {
+  //     'size-200': '200% 200%',
+  //   },
+  // backgroundPosition: {
+  //   'pos-0': '0% 0%',
+  //   'pos-100': '100% 100%',
+  //   },
+    extend: {
+      boxShadow: {
+        '3xl': '2px 2px 2px 1px rgba(0, 0, 0, 0.3)',
+      },
     },
     extend: {
       colors: {
         myTeal: '#00C4D4',
-        lightgreen: '#E7F740',
+        lightgreen: '#a2f740',
         myindigo: '#4F56F6',
         myindigo2: '#4045C3',
         hotpink: '#F11AA8',
         lightBlue: '#C9F8F6',
         navy: '#0D052E',
+        lightBlue: '#C9F8F6',
         gradient:'linear-gradient(to right, #4F56F6, #F11AA)',
       },
       backgroundImage: {
